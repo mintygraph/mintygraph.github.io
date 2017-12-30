b354c3ecdc1acae0b4ccf4063661c3038e0de890
denny0489 <denny0489@gmail.com>
2017-12-29 16:42:11 -0800

    adding background image

--- js/weather.js
+++ js/weather.js
@@ -4,43 +4,62 @@ var lon = -122.4194;
 var city = "San Francisco";
 var unit = 'C';
 var winDir = 'N'
-$.when($.getJSON("http://freegeoip.net/json/")).done(function(locations){
-    if(locations['city'] !== ''){
+$.when($.getJSON("http://freegeoip.net/json/")).done(function (locations) {
+    if (locations['city'] !== '') {
         lat = locations['latitude'];
         lon = locations['longitude'];
         city = locations['city'];
     }
-    $.when($.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon)).done(function(weatherData){
+    $.when($.getJSON("https://fcc-weather-api.glitch.me/api/current?lat=" + lat + "&lon=" + lon)).done(function (weatherData) {
         $(document).ready(function () {
+            console.log(weatherData)
             var windDirData = weatherData['wind']['deg'];
-            if(12.5 < windDirData || windDirData <= 77.5){
+            if (12.5 < windDirData || windDirData <= 77.5) {
                 windDir = 'NE';
             }
-            else if(77.5 < windDirData || windDirData <= 102.5){
+            else if (77.5 < windDirData || windDirData <= 102.5) {
                 windDir = 'E';
             }
-            else if(102.5 < windDirData || windDirData <= 147.5){
+            else if (102.5 < windDirData || windDirData <= 147.5) {
                 windDir = 'SE';
             }
-            else if(147.5 < windDirData || windDirData <= 192.5){
+            else if (147.5 < windDirData || windDirData <= 192.5) {
                 windDir = 'S';
             }
-            else if(192.5 < windDirData || windDirData <= 237.5){
+            else if (192.5 < windDirData || windDirData <= 237.5) {
                 windDir = "SW";
             }
-            else if(237.5 < windDirData || windDirData <= 282.5){
+            else if (237.5 < windDirData || windDirData <= 282.5) {
                 windDir = "W";
             }
-            else if(282.5 < windDirData || windDirData <= 327.5){
+            else if (282.5 < windDirData || windDirData <= 327.5) {
                 windDir = "NW";
             }
-            $('#windInfo').html(weatherData['wind']['speed']+
-                '<a>'+' mph'+'</a>'+' <a>' + windDir+ '&deg;' +'</a>');
+            switch (weatherCond(weatherData)) {
+                //weather conditions from openweathermap.org/weather-conditions
+                case 2: //thunderstorm,
+                    $('html').css("background-image", "url(../img)");
+                    break;
+                case 3: // drizzle
+                    break;
+                case 5: //rain
+                    break;
+                case 6: //snow
+                    break;
+                case 7: //atmosphere
+                    break;
+                case 8: //clear / clouds
+                    break;
+                case 9: //extreme
+                    break;
+            }
+            $('#windInfo').html(weatherData['wind']['speed'] +
+                '<a>' + ' mph' + '</a>' + ' <a>' + windDir + '&deg;' + '</a>');
             $('#weather').html(weatherData['weather'][0]['main']);
             $('#location').html(city);
             $('#tempVal').html('' + weatherData['main']['temp'] + '&deg;' + unit);
-            $('#minTempVal').html(''+weatherData['main']['temp_min'] + '&deg;' + unit);
-            $('#maxTempVal').html(''+weatherData['main']['temp_max'] + '&deg;' + unit);
+            $('#minTempVal').html('' + weatherData['main']['temp_min'] + '&deg;' + unit);
+            $('#maxTempVal').html('' + weatherData['main']['temp_max'] + '&deg;' + unit);
         })
     });
 });
@@ -52,6 +71,11 @@ function switchTemp(num, unit) {
     } else if (unit == 'C') {
 
     }
+}
+
+function weatherCond(weatherID) {
+    var weatherCode = '' + weatherID['weather'][0]['id'];
+    return weatherCode[0];
 }
 
 
