ee306e1f6006a00b404dd06b8b6d7f4a17a938c9
denny0489 <denny0489@gmail.com>
2018-01-11 23:39:15 -0800

    Completed Wikiviewer
    Added links, fixed the undefined error,

--- js/wikiviewer.js
+++ js/wikiviewer.js
@@ -1,49 +1,61 @@
-//TODO
-//Add new function to get the value of the searchinputbox
 $(document).ready(function () {
-    function getSearchInput(){
-        return $('.search-input').val();
-    }
-    var originalResultHTML = $('#result-content-wrapper').html();
+    var originalResultHTML = $('#result-content').html();
+    var searchLink = "https://en.wikipedia.org//w/" +
+        "api.php?action=query&format=json&list=" +
+        "search&callback=%3F&callback=?&srsearch=";
+    var searchTerm;
+
+    function wikiQuery(link) {
+        $.getJSON(link, function (data) {
+                console.log(data);
+                var results = data.query.search;
+                if (results.length > 0) {
+                    for (var i = 0; i < 10; i++) {
+                        $("#result-content").html($('#result-content').html() +
+                            "<a class='result' href='https://en.wikipedia.org/wiki/" +results[i]['title']+
+                            "'><div class='jumbotron'><h1 class='display-4'>" +
+                            results[i]['title'] + "</h1><hr class=\"my-4\"><p class=\"lead\">" +
+                            results[i]['snippet'] + "</p></div></a>")
+                    }
+                } else {
+                    $("#result-content").html("<div class='jumbotron'><h1 class='display-4'>Uh oh, we couldn't" +
+                        " find what you were looking for. Please make another search.</h1></div>")
+                }
+                $("#result-content").fadeIn('slow');
+            $('#input-search-new').val(searchTerm);
+                $('#input-search-new').focus();
+            }
+        )
+    };
     $("#search-content-wrapper").css('display', 'flex').hide().fadeIn('slow', function () {
-        $(".search-input").focus();
+        $("#input-search-main").focus();
     });
-    $(".search-input").keyup(function (event) {
-        if (event.keyCode === 13 && $('.search-input').val() != "") {
+    $("#input-search-main").keyup(function (event) {
+        if (event.keyCode === 13 && $('#input-search-main').val().length) {
             $('#btn-search-main').click();
         }
     });
     $("#btn-search-main").click(function () {
-        var searchTerm = $('.search-input').prop('value');
-        if (searchTerm != "") {
-            $("#search-content-wrapper").fadeOut('slow');
-            $.getJSON("https://en.wikipedia.org//w/" +
-                "api.php?action=query&format=json&list=" +
-                "search&callback=%3F&callback=?&srsearch=" + searchTerm, function (data) {
-                    var results = data.query.search;
-                    for (var i = 0; i < 10; i++) {
-                       $("#result-content-wrapper").html($('#result-content-wrapper').html()+
-                           "<div class='jumbotron'><h1 class='display-4'>"+
-                           results[i]['title']+
-                           "</h1><hr class=\"my-4\"><p class=\"lead\">"+
-                           results[i]['snippet']+ "</p></div>")
-                    }
-                    $("#result-content-wrapper").fadeIn('slow');
-                }
-            )
+        if ($('#input-search-main').val().length) {
+            searchTerm = $('#input-search-main').val();
+            $("#search-content-wrapper").fadeOut('slow', function () {
+                $("#result-content-wrapper").fadeIn('slow');
+            });
+            wikiQuery(searchLink + searchTerm);
         }
     });
-    $(document).on('keyup','.search-input',function(event){
-        if (event.keyCode === 13 && $('.search-input').val() != "") {
+    $(document).on('keyup', '#input-search-new', function (event) {
+        if (event.keyCode === 13 && $('#input-search-new').val().length) {
             $("#btn-search-result").click();
         }
     });
-    $(document).on('click','#btn-search-result',function(){
-        $("#result-content-wrapper").html(originalResultHTML);
-        var ech = getSearchInput();
-
-        if (ech != ""){
-            console.log(ech);
+    $(document).on('click', '#btn-search-result', function () {
+        if($('#input-search-new').val().length){
+            searchTerm = $('#input-search-new').val();
+            $("#result-content").fadeOut('slow', function () {
+                $("#result-content").html(originalResultHTML);
+                wikiQuery(searchLink + searchTerm);
+            });
         }
     });
 })
